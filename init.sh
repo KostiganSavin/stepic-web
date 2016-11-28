3be0d508d134981e589f7861ea1250a4f7c60c0b
KostiganSavin <kostigan.savin@gmail.com>
2016-11-28 16:18:01 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo gunicorn -b 0.0.0.0:8080 --workers=2 hello:app
+#sudo gunicorn -b 0.0.0.0:8080 --workers=2 hello:app
 sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
@@ -7,3 +8,5 @@ sudo /etc/init.d/nginx restart
 #source web/myvenv/bin/activate
 gunicorn -b 0.0.0.0:8080 --pythonpath /home/box/web/ hello:app &
 gunicorn -b 0.0.0.0:8000 --pythonpath /home/box/web/ask ask.wsgi:application &
+
+#sudo /etc/init.d/mysql restart
