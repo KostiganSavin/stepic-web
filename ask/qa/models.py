25919b5d215c6655533e30c5f2c91918e16f8bbd
Konsatntin Savin <kostiga.savin@gmail.com>
2016-11-30 15:37:05 +0300

    clean models in pycharm

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,25 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
-import date
+import datetime
 
 # Create your models here.
-Class Question(models.Model):
+class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateField(balnk=True)
+    added_at = models.DateField(default=datetime.date.today())
     rating = models.IntegerField()
     author = models.ForeignKey(User)
     likes = models.ManyToManyField(User)
 
     def new(self):
-        return self.filter(added_at=date(date.date(now)))
+        return self.filter(added_at=datetime.date.today())
 
     def popular(self):
         return self.order_by('rating')
 
 
-Class Answer(models.Model):
-    text = models.TetxField()
+class Answer(models.Model):
+    text = models.TextField()
     added_at = models.DateField()
-    question = ForeignKey(Question)
+    question = models.ForeignKey(Question)
     author = models.ForeignKey(User)
